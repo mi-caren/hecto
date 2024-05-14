bcd76527ce424b6aa12328bd1993dccbfbb834ce
Michele Carenza <michele.carenza@proton.me>
2024-05-14 11:13:09 +0200

    use crossterm to read keys

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 
 
@@ -14,22 +14,22 @@ impl Editor {
     pub fn run(&self) {
         enable_raw_mode().unwrap();
 
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
 
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03}\r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        },
+                        _ => (),
                     }
-
-                    if c == 'q' {
-                        break;
-                    }
-                }
+                },
                 Err(err) => println!("Error: {}", err),
+                _ => (),
             }
         }
 
