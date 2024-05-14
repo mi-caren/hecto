5baa5f76db6ff218a0550692c4f363665cbde036
Michele Carenza <michele.carenza@proton.me>
2024-05-14 11:26:40 +0200

    clippy

--- src/editor.rs
+++ src/editor.rs
@@ -17,18 +17,15 @@ impl Editor {
         loop {
             match read() {
                 Ok(Key(event)) => {
-                    println!("{:?} \r", event);
+                    println!("{event:?} \r");
 
-                    match event.code {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),
+                    if let Char(c) = event.code {
+                        if c == 'q' {
+                            break;
+                        }
                     }
                 },
-                Err(err) => println!("Error: {}", err),
+                Err(err) => println!("Error: {err}"),
                 _ => (),
             }
         }
